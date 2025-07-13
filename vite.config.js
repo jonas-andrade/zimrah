90c80f9d2d3d12cbfb505c05d16b2d0b7f8d9b7f
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-13 19:01:39 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
-  base: "/", // domínio próprio
   plugins: [react()],
+  base: "/", // ✅ essencial para domínio próprio
 })
