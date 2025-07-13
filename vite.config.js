3dd7e6c8d328d388d90bfde65faa358ea6dfbf57
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-13 19:14:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/zimrah.io/", // use este se estiver em https://usuario.github.io/zimrah-frontend/
+  base: "/zimrah", // use este se estiver em https://usuario.github.io/zimrah-frontend/
 })
