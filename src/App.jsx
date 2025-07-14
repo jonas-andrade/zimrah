3e9182d5159207a1d2f32fbec727c2a709029720
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-14 18:30:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-
-// import { useAuthStore } from './stores/authStore'; // Não precisa mais
 
 export default function App() {
   return (
     <Router basename="/zimrah">
       <Routes>
+        {/* Ao acessar "/", redireciona direto para "/dashboard" */}
         <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        {/* Rota do dashboard sem proteção */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
